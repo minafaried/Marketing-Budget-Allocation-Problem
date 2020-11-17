760d6fbeeb00ee49753ad9102534453931245bec
mina faried <36136028+minafaried@users.noreply.github.com>
2020-11-18 00:11:54 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import random
+
+
+def init(population_num, marketing_channels_num,budget):  #mina
+    pass
+
+
+def check(population, marketing_channels_num,budget,investment_lower_upper): #mustafa
+    pass
+
+
+def fitness_and_selection(population, marketing_channels_num, ROI, selectionNumber): #mustafa
+    pass
+
+
+def crossover(selection): #mina
+    pass
+
+
+def mutation_uniform(bit_filp, crossover,marketing_channels_num, investment_lower_upper): #peter
+    pass
+def mutation_non_uniform(bit_filp, crossover,marketing_channels_num, investment_lower_upper): #peter
+    pass
+
+def replacement(population, mutation,marketing_channels_num,investment_lower_upper,budget): #mina
+    pass
+
+
+def select_the_fittest(population,marketing_channels_num,investment_lower_upper,budget):  #peter
+    pass
+
+
+def genetic_algo(budget, marketing_channels_num, marketing_channels, ROI, investment_lower_upper): #mina
+    pass
+
+def MBAP_input(): #mustafa
+    pass
+
+def MBAP(): #mustafa
+    pass
+
+def final_result(): #mustafa
+   pass
